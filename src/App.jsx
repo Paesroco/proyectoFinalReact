ec76d874ebb54201955f3f92608f462538fecaee
AlvaroAlzaga <alvaroalzaga689@gmail.com>
2025-02-10 20:03:55 -0300

    se cambia a espanhol(no tengo enie)

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,91 @@
 import React, { useState } from "react";
 import "./style.css";
 
-
 function App() {
-  const [quantity, setQuantity] = useState(0);
+  const [cantidad, setCantidad] = useState(0);
+  const [contadorCarrito, setContadorCarrito] = useState(0);
 
-  const handleQuantityChange = (action) => {
-    if (action === "increase") {
-      setQuantity(quantity + 1);
-    } else if (action === "decrease" && quantity > 0) {
-      setQuantity(quantity - 1);
+  const manejarCambioCantidad = (accion) => {
+    if (accion === "aumentar") {
+      setCantidad(cantidad + 1);
+    } else if (accion === "disminuir" && cantidad > 0) {
+      setCantidad(cantidad - 1);
     }
   };
 
-  const addToCart = () => {
-    alert(`Added ${quantity} items to the cart`);
+  const agregarAlCarrito = () => {
+    if (cantidad > 0) {
+      setContadorCarrito(contadorCarrito + cantidad);
+      setCantidad(0);
+    }
   };
 
   return (
     <div>
       <header>
         <nav className="navbar">
-          <div className="logo">sneakers</div>
+          <div className="logo">zapatillas</div>
           <ul className="nav-links">
-            <li><a href="#">Collections</a></li>
-            <li><a href="#">Men</a></li>
-            <li><a href="#">Women</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="#">Colecciones</a></li>
+            <li><a href="#">Hombres</a></li>
+            <li><a href="#">Mujeres</a></li>
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#">Contacto</a></li>
           </ul>
-          <div className="cart-user">
-            <div className="cart-icon">
-              <img src="./images/icon-cart.svg" alt="Cart" />
+          <div className="carrito-usuario">
+            <div className="icono-carrito">
+              <img src="./images/icon-cart.svg" alt="Carrito" />
+              {contadorCarrito > 0 && <span className="contador-carrito">{contadorCarrito}</span>}
             </div>
-            <img className="user-avatar" src="./images/image-avatar.png" alt="User Avatar" />
+            <img className="avatar-usuario" src="./images/image-avatar.png" alt="Avatar del usuario" />
           </div>
         </nav>
       </header>
 
       <main>
-        <div className="product">
-          <div className="product-image">
-            <img src="./images/image-product-1.jpg" alt="Sneaker" />
-            <div className="thumbnails">
-              <img src="./images/image-product-1-thumbnail.jpg" alt="Thumbnail 1" />
-              <img src="./images/image-product-2-thumbnail.jpg" alt="Thumbnail 2" />
-              <img src="./images/image-product-3-thumbnail.jpg" alt="Thumbnail 3" />
-              <img src="./images/image-product-4-thumbnail.jpg" alt="Thumbnail 4" />
+        <div className="producto">
+          <div className="imagen-producto">
+            <img src="./images/image-product-1.jpg" alt="Zapatilla" />
+            <div className="miniaturas">
+              <img src="./images/image-product-1-thumbnail.jpg" alt="Miniatura 1" />
+              <img src="./images/image-product-2-thumbnail.jpg" alt="Miniatura 2" />
+              <img src="./images/image-product-3-thumbnail.jpg" alt="Miniatura 3" />
+              <img src="./images/image-product-4-thumbnail.jpg" alt="Miniatura 4" />
             </div>
           </div>
 
-          <div className="product-details">
-            <h2 className="company">Sneaker Company</h2>
-            <h1 className="product-title">Fall Limited Edition Sneakers</h1>
-            <p className="product-description">
-              These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.
+          <div className="detalles-producto">
+            <h2 className="compania">Compañía de Zapatillas</h2>
+            <h1 className="titulo-producto">Zapatillas Edición Limitada Otoño</h1>
+            <p className="descripcion-producto">
+              Estas zapatillas de perfil bajo son el compañero perfecto para tu ropa casual. Con una suela exterior de goma duradera, resistirán todo lo que el clima pueda ofrecer.
             </p>
-            <div className="price-section">
-              <div className="price">
-                <span className="current-price">$125.00</span>
-                <span className="discount">50%</span>
+            <div className="seccion-precio">
+              <div className="precio">
+                <span className="precio-actual">$125.00</span>
+                <span className="descuento">50%</span>
               </div>
-              <span className="original-price">$250.00</span>
+              <span className="precio-original">$250.00</span>
             </div>
-            <div className="cart-section">
-              <div className="quantity-control">
+            <div className="seccion-carrito">
+              <div className="control-cantidad">
                 <button 
-                  className="decrease" 
-                  onClick={() => handleQuantityChange("decrease")}
+                  className="disminuir" 
+                  onClick={() => manejarCambioCantidad("disminuir")}
                 >
                   -
                 </button>
-                <span className="quantity">{quantity}</span>
+                <span className="cantidad">{cantidad}</span>
                 <button 
-                  className="increase" 
-                  onClick={() => handleQuantityChange("increase")}
+                  className="aumentar" 
+                  onClick={() => manejarCambioCantidad("aumentar")}
                 >
                   +
                 </button>
               </div>
-              <button className="add-to-cart" onClick={addToCart}>
-                <img src="./images/icon-cart.svg" alt="Cart Icon" />
-                Add to cart
+              <button className="agregar-al-carrito" onClick={agregarAlCarrito}>
+                <img src="./images/icon-cart.svg" alt="Icono de Carrito" />
+                Agregar al carrito
               </button>
             </div>
           </div>
